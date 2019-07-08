fe9953459ca47936e4f468ebb054129a5ba6cbe7
Natasha Langston <langstonnatasha@gmail.com>
2019-07-08 01:00:24 -0400

    Updated Deliverable

--- src/WhatToEat.java
+++ src/WhatToEat.java
@@ -8,36 +8,40 @@ public class WhatToEat {
 
 		String eventType = "casual";
 		int partySize = 1;
-		String result = eventType + partySize;
+		String mealSuggestion = ("sandwiches ");
+		String prepSuggestion = ("in your kitchen.");
+		String result = mealSuggestion;
 
 		System.out.println("What type of event are you having? ");
 
 		eventType = scnr.next();
 
 		if (eventType.equals("casual")) {
-			result = "sandwiches";
+			mealSuggestion = "sandwiches ";
 		} else if (eventType.equals("semi-formal")) {
-			result = "fried chicken";
+			mealSuggestion = "fried chicken ";
 		} else if (eventType.contentEquals("formal")) {
-			result = "chicken parmesan";
+			mealSuggestion = "chicken parmesan ";
 
 		}
 
 		System.out.println("What's the size of your party? ");
+
 		partySize = scnr.nextInt();
 
 		if (partySize == 1) {
-			result = "in the microwave.";
+			prepSuggestion = "in the microwave.";
 		} else if (partySize == 2 && partySize <= 12) {
-			result = ("in your kitchen.");
-		} else if (partySize == 13 && partySize > 13) {
-			result = ("by the caterer.");
+			prepSuggestion = ("in your kitchen.");
+		} else if (partySize >= 13) {
+			prepSuggestion = ("by a caterer.");
 
 		}
+
+		result = mealSuggestion + "prepared " + prepSuggestion;
 
 		System.out.print("Since you're hosting a " + eventType + " event ");
 		System.out.print("for " + partySize + " participants, you should serve " + result);
 	}
 
-	// scnr.close(); }
 }
